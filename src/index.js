c9f382d801c9d3f0f30146b37c8e577573081542
Iván (profe) <ivan@adalab.es>
2021-11-12 20:19:50 +0100

    Add styling, remove logs.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const server = express();
 // Configuramos el servidor
 server.use(cors());
 server.use(express.json({ limit: '50mb' }));
+server.use(express.urlencoded());
 server.set('view engine', 'ejs');
 
 // Arrancamos el servidor en el puerto indicado o en 3000
@@ -28,13 +29,9 @@ server.delete("/", showParams);
 
 // Common functions
 function showParams(req, res) {
-  console.log(req.query);
-  console.log(req.body);
-  console.log(req.method);
-
   res.render('showParams', {
     method: req.method,
     queryParams: req.query,
     bodyParams: req.body
-  })
+  });
 }
